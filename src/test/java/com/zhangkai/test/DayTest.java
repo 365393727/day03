5136400a3ab9827b7c742d882fefc82a6eff7975
zhangkai <365393727@qq.com>
2019-09-05 08:13:43 +0800

    第一次提交

---
+++ src/test/java/com/zhangkai/test/DayTest.java
@@ -0,0 +1,28 @@
+/**   
+ * Copyright © 2019 公司名. All rights reserved.
+ * 
+ * @Title: DayTest.java 
+ * @Prject: zhangkai-day03
+ * @Package: com.zhangkai.test 
+ * @Description: TODO
+ * @author: 张凯   
+ * @date: 2019年9月5日 上午8:10:30 
+ * @version: V1.0   
+ */
+package com.zhangkai.test;
+
+/** 
+ * @ClassName: DayTest 
+ * @Description: TODO
+ * @author: 张凯
+ * @date: 2019年9月5日 上午8:10:30  
+ */
+public class DayTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("helloWord");
+		
+	}
+	
+}
